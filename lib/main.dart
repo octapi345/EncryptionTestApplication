67d4f7ae6adbfa8ae445a9b0515f45a07bed1546
octapi345 <djhorton2002@gmail.com>
2023-11-01 23:56:53 -0400

    cryptography dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:cryptography/cryptography.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 
 Future main() async {
   await Firebase.initializeApp();
@@ -8,7 +10,6 @@ Future main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
