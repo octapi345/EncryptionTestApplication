ca438ddec32d8338186cf05dc83eb3948fe36923
regvsn <reginald.appiah7@gmail.com>
2023-11-03 20:27:39 -0400

    Fixed Setup Error

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:cryptography/cryptography.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
 Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
