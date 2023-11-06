19b1b349bc35df9e0eae65d925a6f09d09b7ef57
octapi345 <djhorton2002@gmail.com>
2023-11-05 22:49:40 -0500

    fixed firebase options

---
+++ lib/DBMS.dart
@@ -0,0 +1,34 @@
+import 'package:cryptography/cryptography.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    // Replace with actual values
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCxH3S_oA1Cn7ANe5FIHayG0Qad_utNM2o",
+      appId: "1:893923649022:android:5b086703888e79f47eccaf",
+      messagingSenderId: "893923649022",
+      projectId: "standardized-payment-encrypt",
+    ),
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:cryptography/cryptography.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    // Replace with actual values
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCxH3S_oA1Cn7ANe5FIHayG0Qad_utNM2o",
+      appId: "1:893923649022:android:5b086703888e79f47eccaf",
+      messagingSenderId: "893923649022",
+      projectId: "standardized-payment-encrypt",
+    ),
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
